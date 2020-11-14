da9ba70af53a710048be86e9a7a46a87d2870066
unknown <felixedmundoh@hotmail.com>
2020-11-14 22:18:32 +0000

    possible final commit

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
 (async function loadCountries(url) {
     async function getData(url) {
         const response = await fetch(url);
+        debugger
         const data = await response.json();
 
 
         return data;
     }
 
-    const dataCountries = await getData('https://restcountries.eu/rest/v2/all');
-    console.log(dataCountries);
+    const baseCountries = await getData('https://restcountries.eu/rest/v2/');
+
 
 
 
@@ -19,7 +20,7 @@
             <div class="primaryPlaylistItem-image">
             <img src="${country.flag}" width="250" height="250">
             </div>
-            <h2>Name : ${country.name}</h2>
+            <h2>${country.name}</h2>
             <p> Capital : ${country.capital}</p>
             <p> Continent : ${country.region}</p>
             <p> Population : ${country.population}</p>
@@ -30,9 +31,22 @@
         `)
     }
 
+    let $form = document.querySelector('#form');
+    $form = addEventListener('submit', async(e) => {
+        e.preventDefault();
+        console.log('testing');
+
+        const data = new FormData(document.getElementById('form'));
+        console.log(data.get('name'));
+
+        const searchCountry = await getData(`${baseCountries}name/${data.get('name')}`);
+        console.log(searchCountry.name);
+
+    })
+
     let $countries = document.querySelector('#countries');
 
-    dataCountries.forEach((country) => {
+    baseCountries.forEach((country) => {
 
 
         const htmlString = countryTemplate(country);
@@ -43,5 +57,7 @@
 
         $countries.append(html.body.children[0]);
     })
+
+
 
 })()
---
+++ js/index2.js
@@ -0,0 +1,115 @@
+(async function loadCountries(url) {
+    async function getData(url) {
+        const response = await fetch(url);
+
+        const data = await response.json();
+
+
+        return data;
+    }
+
+    const baseCountries = 'https://restcountries.eu/rest/v2/';
+
+
+
+
+
+    function countryTemplate(country) {
+        return (
+            `<div class="cardItem">
+            <div class="primaryPlaylistItem-image">
+            <img src="${country.flag}" width="250" height="250">
+            </div>
+            <h2>${country.name}</h2>
+            <p> Capital : ${country.capital}</p>
+            <p> Continent : ${country.region}</p>
+            <p> Population : ${country.population}</p>
+            <p> Languages : ${country.languages[0].name} </p>
+            
+            </div>
+        
+        `)
+    }
+
+    let $form = document.querySelector('#form');
+    $form = addEventListener('submit', async(e) => {
+
+            e.preventDefault();
+            cleanHTML();
+
+            console.log('testing');
+
+            const data = new FormData(document.getElementById('form'));
+            console.log(data.get('name'));
+
+            const searchCountry = await getData(`${baseCountries}name/${data.get('name')}`);
+            console.log(searchCountry);
+
+
+
+
+            searchCountry.forEach((country) => {
+                let $container = document.querySelector('#countries');
+                const htmlString = countryTemplate(country);
+
+                const html = document.implementation.createHTMLDocument();
+
+                html.body.innerHTML = htmlString;
+
+                $container.append(html.body.children[0]);
+
+            })
+
+        })
+        // let $region = document.querySelector('#region a')
+        // $region = addEventListener('click', (e) => {
+        //     e.preventDefault();
+        //     console.log($region.value);
+        // })
+
+    const $region = document.querySelector('#region');
+    $region.addEventListener('change', async(e) => {
+        let regionData;
+        cleanHTML();
+        let region = e.target.value;
+        if (region === 'all') {
+            regionData = await getData(`${baseCountries}${region}`)
+        } else {
+
+            regionData = await getData(`${baseCountries}region/${region}`)
+        }
+        renderCountries(regionData, $countries);
+    })
+
+    function cleanHTML() {
+        while ($countries.firstChild) {
+            $countries.removeChild($countries.firstChild)
+        }
+
+    }
+
+
+    let $countries = document.querySelector('#countries');
+    let countries = await getData(`${baseCountries}all`)
+    console.log(countries);
+
+    function renderCountries(list, $container) {
+
+
+        list.forEach((country) => {
+
+
+            const htmlString = countryTemplate(country);
+
+            const html = document.implementation.createHTMLDocument();
+
+            html.body.innerHTML = htmlString;
+
+            $container.append(html.body.children[0]);
+
+
+        })
+    }
+    renderCountries(countries, $countries)
+
+})()
